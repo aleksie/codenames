c380c8badf4ed6c38943556a9c9241ba0e65d721
hukuuu <hukuuu@gmail.com>
2016-09-07 18:07:00 +0300

    styling + format

--- components/Board/Board.js
+++ components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import cx from 'classnames';
 import s from './styles.css';
 import Button from '../Button';
@@ -9,123 +9,156 @@ import types from './types.json';
 
 class Board extends React.Component {
 
-    constructor() {
-        super();
-        this.generateNewGameState = this.generateNewGameState.bind(this);
-        this.checkWinner = this.checkWinner.bind(this);
-        this.types = types.types;
-        this.words = words.words;
-        let cards = this.generateNewGameState();
-        this.state = {
-            cards: cards,
-            isLegendShowing: false
-        };
-     }
+  constructor() {
+    super();
+    this.generateNewGameState = this.generateNewGameState.bind(this);
+    this.checkWinner = this.checkWinner.bind(this);
+    this.legendClick = this.legendClick.bind(this)
+    this.newGameClick = this.newGameClick.bind(this)
+    this.checkWinner = this.checkWinner.bind(this)
+    this.cardClick = this.cardClick.bind(this)
 
-    generateNewGameState() {
-        console.log("generateNewGameState");
-        let cards = [];
-
-        this.isRedFirst = !!Math.round(Math.random() * 1);
-        let lastWord = this.isRedFirst ? "RED" : "BLUE";
-
-        this.types[24] = lastWord;
-
-        console.log(this.types);
-        console.log(types);
-        let wordsIndexes = this.getRandomIndexes(this.words.length - 1);
-        let typeIndexes = this.getRandomIndexes(this.types.length - 1);
-        console.log(wordsIndexes);
-        console.log(typeIndexes);
-        for (var i=0; i < 25; i++) {
-            let card = {
-                id: i,
-                text: this.words[wordsIndexes[i]],
-                type: (this.types[typeIndexes[i]]),
-                revealed: false
-            };
-            cards.push(card);
-
-    }
-        return cards;
-    }
-
-    getRandomIndexes(max) {
-        let indexes = [];
-        for (var i=0; i < 25; i++) {
-            let index = Math.floor(Math.random() * (max + 1));
-            if (indexes.indexOf(index) == -1) {
-                indexes.push(index);
-            } else {
-                i--;
-            }
-
-        }
-        return indexes;
-
-    }
-
-  static propTypes = {
-  };
-
-  componentDidMount() {
+    this.types = types.types;
+    this.words = words.words;
+    let cards = this.generateNewGameState();
+    console.log("cards", cards);
+    this.state = {
+      cards: cards,
+      isLegendShowing: false
+    };
   }
 
-  componentWillUnmount() {
+  generateNewGameState() {
+    console.log("generateNewGameState");
+    let cards = [];
+
+    this.isRedFirst = !!Math.round(Math.random() * 1);
+    let lastWord = this.isRedFirst ? "RED" : "BLUE";
+
+    this.types[24] = lastWord;
+
+    console.log(this.types);
+    console.log(types);
+    let wordsIndexes = this.getRandomIndexes(this.words.length - 1);
+    let typeIndexes = this.getRandomIndexes(this.types.length - 1);
+    console.log(wordsIndexes);
+    console.log(typeIndexes);
+    for (var i = 0; i < 25; i++) {
+      let card = {
+        id: i,
+        text: this.words[wordsIndexes[i]],
+        type: (this.types[typeIndexes[i]]),
+        revealed: false
+      };
+      cards.push(card);
+
+    }
+    return cards;
+  }
+
+  getRandomIndexes(max) {
+    let indexes = [];
+    for (var i = 0; i < 25; i++) {
+      let index = Math.floor(Math.random() * (max + 1));
+      if (indexes.indexOf(index) == -1) {
+        indexes.push(index);
+      } else {
+        i--;
+      }
+
+    }
+    return indexes;
+
   }
 
   cardClick(id) {
-      let type = ""
+    let type = ""
     let updated = this.state.cards.map(card => {
-        if(card.id === id) {
-            card.revealed = true;
-            type = card.type;
-        }
-        return card
+      if (card.id === id) {
+        card.revealed = true;
+        type = card.type;
+      }
+      return card
     })
-    this.setState ({cards: updated})
+    this.setState({
+      cards: updated
+    })
     this.checkWinner(type);
   }
 
   legendClick() {
-      let updated = this.state;
-      updated.isLegendShowing = !updated.isLegendShowing;
-      this.setState(updated);
+    let updated = this.state;
+    updated.isLegendShowing = !updated.isLegendShowing;
+    this.setState(updated);
   }
 
   newGameClick() {
-      let cards = this.generateNewGameState();
-      let state = {
-          cards: cards,
-          isLegendShowing: false
-      };
+    let cards = this.generateNewGameState();
+    let state = {
+      cards: cards,
+      isLegendShowing: false
+    };
 
-      this.setState(state);
+    this.setState(state);
   }
 
-  checkWinner(type){
-
-      if (type === "YELLOW") {
-          return;
-      }
-      if (type === "BLACK") {
-          alert("Your opponent wins!");
-      } else {
-          //this.state.map(card => )
-          //todo: proveri dali ke najdes karta so tip toj i revealed false
-      }
-
+  checkWinner(type) {
+    if (type === "YELLOW") {
+      return;
+    }
+    if (type === "BLACK") {
+      alert("Your opponent wins!");
+    } else {
+      //this.state.map(card => )
+      //todo: proveri dali ke najdes karta so tip toj i revealed false
+    }
   }
 
   render() {
-      return <div className={s.root}>
-      <input type="button" className="buttonLegend" onClick={this.legendClick.bind(this)} value={this.state.isLegendShowing ? "HIDE LEGEND" : "SHOW LEGEND"} />
-      <input type="button" className="buttonNewGame" onClick={this.newGameClick.bind(this)} value="NEW GAME"/>
-      <br/>
-        {this.state.cards.map((card)=> {
-            return <Card key={card.id} card={card} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick.bind(this)} />
-        })}
-      </div>
+    let i = 0
+
+    return <div>
+              <input type="button" className="buttonLegend" onClick={this.legendClick} value={this.state.isLegendShowing ? "HIDE LEGEND" : "SHOW LEGEND"} />
+              <input type="button" className="buttonNewGame" onClick={this.newGameClick} value="NEW GAME"/>
+              <br/>
+              <div className={s.container}>
+                <div className={s.row}>
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                </div>
+                <div className={s.row}>
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                </div>
+                <div className={s.row}>
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                </div>
+                <div className={s.row}>
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                </div>
+                <div className={s.row}>
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                  <Card card={this.state.cards[i++]} isLegendShowing={this.state.isLegendShowing} onClick={this.cardClick} />
+                </div>
+              </div>
+           </div>
   }
 
 }
--- components/Card/Card.js
+++ components/Card/Card.js
@@ -4,44 +4,40 @@ import s from './styles.css';
 
 class Card extends React.Component {
 
-  static propTypes = {
-  };
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-      this.props.onClick(this.props.card.id);
- }
+    this.props.onClick(this.props.card.id);
+  }
 
   render() {
-      var map = {
-          'RED' : s.red,
-          'BLUE' : s.blue,
-          'YELLOW': s.yellow,
-          'BLACK': s.black
-      }
-      var overlay = cx({
-          [s.card] : true,
-          [map[this.props.card.type]]: this.props.card.revealed || this.props.isLegendShowing
-       });
 
-      return <div className={overlay}  onClick={this.handleClick.bind(this)}>
-                <div className={s.cardInner}>
-                    <div className={s.cardUpper}>
-                        <div className={s.cardCircle}>
-                            <svg width="100%" height="20" xmlns="http://www.w3.org/2000/svg"><g><title>background</title><rect fill="#eadaca" id="canvas_background" height="100%" width="100%" y="-1" x="-1"></rect></g><g><title>Layer 1</title><ellipse ry="8" rx="8" id="svg_2" cy="50%" cx="50%" stroke="#b8a48b" fill="#eadaca"></ellipse><ellipse ry="6" rx="6" id="svg_1" cy="50%" cx="50%" stroke="#b8a48b" fill="#fffdfc"></ellipse></g></svg>
-                        </div>
-                        <div className={s.cardImage}></div>
-                        <div className={s.cardGrey}> {this.props.card.text.toUpperCase()} </div>
-                    </div>
-                    <div className={s.cardLower}> {this.props.card.text.toUpperCase()} </div>
-                </div>
+    var map = {
+      'RED': s.red,
+      'BLUE': s.blue,
+      'YELLOW': s.yellow,
+      'BLACK': s.black
+    }
 
+    var cardStyle = cx({
+      [s.card]: true,
+      [map[this.props.card.type]]: this.props.card.revealed || this.props.isLegendShowing
+    });
+
+    return (<div className={cardStyle} onClick={this.handleClick}>
+        <div className={s.inner}>
+          <div className={s.upper}>
+            <div className={s.circle}>
+              <svg width="100%" height="20" xmlns="http://www.w3.org/2000/svg"><g><title>background</title><rect fill="#eadaca" id="canvas_background" height="100%" width="100%" y="-1" x="-1"></rect></g><g><title>Layer 1</title><ellipse ry="8" rx="8" id="svg_2" cy="50%" cx="50%" stroke="#b8a48b" fill="#eadaca"></ellipse><ellipse ry="6" rx="6" id="svg_1" cy="50%" cx="50%" stroke="#b8a48b" fill="#fffdfc"></ellipse></g></svg>
             </div>
+            <div className={s.grey}><span className={s.text}>{this.props.card.text.toUpperCase()}</span></div>
+          </div>
+          <div className={s.lower}><span className={s.text}>{this.props.card.text.toUpperCase()}</span></div>
+        </div>
+      </div>)
   }
 
 }
